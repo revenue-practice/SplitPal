7362277a0aba957d574687ccd57278360df4ae8f
developerriyansh-flow2125 <developerriyansh@gmail.com>
2025-12-01 13:00:06 +0530

    base layout for server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+require('dotenv').config();
 const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
