c50120dc124e74d1d801905f22e73d6c27badea5
developerriyansh-flow2125 <developerriyansh@gmail.com>
2025-12-02 19:10:38 +0530

    pre commit -expenses

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/auth.route');
 const groupsRouter = require('./routes/groups.route');
+const expensesRouter = require('./routes/expenses.route');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -13,6 +14,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/auth', authRouter);
 app.use('/groups', groupsRouter);
+app.use('/expenses', expensesRouter);
+
+app.get('/', () => {
+    res.send('App is running');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
