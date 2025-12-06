d6a6f7f3469f2bbc9bf69936f96eb12b99610dec
developerriyansh-flow2125 <developerriyansh@gmail.com>
2025-12-06 17:34:30 +0530

    commit complete - fetch all users from the group

--- controllers/groups.controller.js
+++ controllers/groups.controller.js
@@ -103,7 +103,7 @@ const fetchAllUsers = async (req, res) => {
 
     try { 
         const usersInformation = await fetchAllUsersModel(groupId);
-        if(usersInformation.code === 200) return usersInformation;
+        if(usersInformation.code === 200) return res.status(200).json(usersInformation.data);
         
         errorResponse(res, response);
     }
--- models/groups.fetch.model.js
+++ models/groups.fetch.model.js
@@ -1,13 +1,26 @@
-const { DBTABLES, acceptedSuccessCode, internalServerErrorCode } = require("../utils/constants");
+const { DBTABLES, internalServerErrorCode, acceptedSuccessCode } = require("../utils/constants");
 const { executeAsyncQueryWithoutLock } = require("../utils/helper");
 
 const fetchAllUsersModel = async (groupId) => {
-    const query = `SELECT * FROM ${DBTABLES.groupMembers} WHERE group_id = $1 LIMIT 100`;
+    const getUsersQuery = `SELECT id, user_id FROM ${DBTABLES.groupMembers} WHERE group_id = $1 and is_active = True LIMIT 100`;
     try {
-        const response = await executeAsyncQueryWithoutLock(query, [groupId]);
-        if(response.rowCount) return {
-            code: 200,
-            data: response.rows
+        const fetchUserIdsResponse = await executeAsyncQueryWithoutLock(getUsersQuery, [groupId]);
+        if(fetchUserIdsResponse.rowCount) {
+            let baseUserDetailsQuery = `SELECT fname, lname, email FROM ${DBTABLES.users} WHERE id in (`;
+            let paramsCount = 1, userDetailQueryParams = [];
+            for (let user of fetchUserIdsResponse.rows) {
+                const { user_id: userId } = user;
+                baseUserDetailsQuery += `$${paramsCount}, `;
+                userDetailQueryParams.push(userId); paramsCount += 1;
+            }
+            baseUserDetailsQuery.slice(0, -2); 
+            let userDetailQuery = baseUserDetailsQuery.slice(0, -2); userDetailQuery += `) LIMIT 100`;
+            const response = await executeAsyncQueryWithoutLock(userDetailQuery, userDetailQueryParams);
+
+            if(response.rowCount) return {
+                ...acceptedSuccessCode,
+                data: response.rows
+            }
         };
 
         return internalServerErrorCode;
